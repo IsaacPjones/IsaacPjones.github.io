424aeb019f004f3a6b027ed4f07f058aa8400efd
IsaacPjones <ija19@sfu.ca>
2025-06-04 21:49:49 -0700

    Finished setting up enviroment, starting on load screen

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,44 +1,15 @@
-import Navbar from './Navbar'
+
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
+
 
 function App() {
   return (
     <>
-      <Navbar />
-
-      <section id="home" className="h-screen bg-white flex items-center justify-center pt-24">
-        <div className="max-w-7xl mx-auto px-4 text-center">
-          <h1 className="text-8xl font-bold">Welcome to My site</h1>
-        </div>
-      </section>
-
-      <section id="about" className="h-screen bg-gray-100 p-8">
-        <div className="max-w-7xl mx-auto px-4">
-          <h2 className="text-2xl font-semibold mb-4">About Me</h2>
-          <p className="max-w-xl">
-            I'm a student at Simon Fraser University, studying Computer Science and business. I have a passion for software development and enjoy working on projects that challenge my skills and creativity.
-          </p>
-        </div>
-      </section>
-
-      <section id="projects" className="h-screen bg-blue-100 p-8">
-        <div className="max-w-7xl mx-auto px-4">
-          <h2 className="text-2xl font-semibold mb-4">Projects</h2>
-          <ul className="list-disc ml-6">
-            <li>Project 1</li>
-            <li>Project 2</li>
-          </ul>
-        </div>
-      </section>
-
-      <section id="contact" className="h-screen bg-pink-100 p-8">
-        <div className="max-w-7xl mx-auto px-4">
-          <h2 className="text-2xl font-semibold mb-4">Contact</h2>
-          <p>Email: IsaacJones32@Gmail.com</p>
-          <p>LinkedIn: https://www.linkedin.com/in/isaac-jones-8a863a29a/</p>
-        </div>
-      </section>
+      <LoadingScreen />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
