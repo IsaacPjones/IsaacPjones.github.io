e2ea88cfb85c36af277567ddc85d3f32fafa52cd
Isaac Jones <IJA19@sfu.ca>
2025-06-18 10:21:43 -0700

    Populated links to personal pages + Added more personal info
    I cleaned up and altered some text as well as added in my personal links

--- Portfolio/src/components/sections/Home.jsx
+++ Portfolio/src/components/sections/Home.jsx
@@ -9,15 +9,13 @@ export const Home = () => {
         <RevealOnScroll>
         <div className="text-center z-10 px-"> 
             <h1 className="text-xl md:text-8xl font-bold mb-6 bg-cyan-500 bg-clip-text text-transparent leading-left">
-                Welcome to my Web Page 
+                Welcome!
             </h1>
 
             <br></br>
 
             <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-                I'm a student at Simon Fraser University, studying Computer Science and Business. 
-                I enojoy being creative and building things that people can use. In my free time I enojoy
-                photography, watching mixed martial arts, and spending time with friends and family.
+                Im a Computer Science and Business student at Simon Fraser University, driven to build useful, creative, and human centered digital experiences.
             </p>
 
             
